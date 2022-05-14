c7cb5a4e72934c9f4e67ac9e7d9120cd7a38b468
Caio Pessoa <caio.almeida.pessoa07@aluno.ifce.edu.br>
2022-05-13 21:36:45 -0300

    modelo de municao, objeto_tipo e arma criados

--- gun_registrer/models.py
+++ gun_registrer/models.py
@@ -3,3 +3,21 @@ from django.db import models
 
 class Calibre(models.Model):
     desc_calibre = models.CharField(max_length=45)
+
+
+class Arma(models.Model):
+    marca = models.CharField(max_length=64)
+    modelo = models.CharField(max_length=64)
+    quantidade_de_tiros = models.IntegerField()
+    valor_estimado = models.FloatField()
+    imagem = models.CharField(max_length=128)
+
+
+class Municao(models.Model):
+    marca = models.CharField(max_length=64)
+    modelo = models.CharField(max_length=64)
+    valor_estimado = models.FloatField()
+
+
+class objeto_tipo:
+    tipo_de_objeto = models.CharField(max_length=64)
